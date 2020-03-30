197138914fb6599f2d5472ae1fcd9fcae8e73cd4
Patrick <patrick.alves@accenture.com>
2020-03-30 13:25:06 -0300

    ajuste validacao tela validador

--- src/app/validador/validador.component.ts
+++ src/app/validador/validador.component.ts
@@ -22,6 +22,11 @@ export class ValidadorComponent implements OnInit {
   cns: string;
   cnpj: string;
 
+
+  msgCpf;
+  msgCns;
+  msgCnpj;
+
   constructor(
     private consultaService: ConsultaService
   ) { }
@@ -29,8 +34,103 @@ export class ValidadorComponent implements OnInit {
   ngOnInit() {
   }
 
-  validar() {
 
+   validarCpf() {
+    if (this.cpf) {
+      this.validaCpf();
+    }
+  }
+
+  validarCnpj() {
+    if (this.cnpj) {
+      this.validaCnpj();
+    }
+  }
+
+  validarCns() {
+    if (this.cns) {
+      this.validaCns();
+    }
+  }
+
+   validaCpf() {
+    this.loading = true;
+    this.consultaService.validaCpf(this.cpf).subscribe(item => {
+      if (item) {
+        this.loading = false;
+        this.msgCpf = item;
+        if (this.msgCpf.status === '1') {
+          this.exibirMsgValido(this.msgCpf.data.message);
+        } else if (this.msgCpf.status === '0') {
+          this.exibirMsgInvalido(this.msgCpf.data.message);
+        }
+      }
+    },
+    erro => {
+      this.validaRetornoApi(erro);
+    }
+    );
+  }
+
+   validaCnpj() {
+    this.loading = true;
+    this.consultaService.validaCnpj(this.cnpj).subscribe(item => {
+      if (item) {
+        this.loading = false;
+        this.msgCnpj = item;
+        if (this.msgCnpj.status === '1') {
+          this.exibirMsgValido(this.msgCnpj.data.message);
+        } else if (this.msgCnpj.status === '0') {
+          this.exibirMsgInvalido(this.msgCnpj.data.message);
+        }
+      }
+    },
+    erro => {
+      this.validaRetornoApi(erro);
+    }
+    );
+  }
+
+   validaCns() {
+    this.loading = true;
+    this.consultaService.validaCns(this.cns).subscribe(item => {
+      if (item) {
+        this.loading = false;
+        this.msgCns = item;
+        if (this.msgCns.status === '1') {
+          this.exibirMsgValido(this.msgCns.data.message);
+        } else if (this.msgCns.status === '0') {
+          this.exibirMsgInvalido(this.msgCns.data.message);
+        }
+      }
+    },
+    erro => {
+      this.validaRetornoApi(erro);
+    }
+    );
+  }
+
+  exibirMsgValido(msgRetorno) {
+    if (msgRetorno) {
+      alert(msgRetorno);
+    } else {
+      return;
+    }
+  }
+
+  exibirMsgInvalido(msgRetorno) {
+    if (msgRetorno) {
+      alert(msgRetorno);
+    } else {
+      return;
+    }
+  }
+
+  validaRetornoApi(erro) {
+    if (erro.error.status === '0') {
+      return console.log(erro.error.data.message);
+    }
+    return;
   }
 
 }
